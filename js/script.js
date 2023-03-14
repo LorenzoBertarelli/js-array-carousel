8fede80e989000d96a87d73515e6aef5a09af54b
LorenzoBertarelli <lorenzobertarelli59@gmail.com>
2023-03-14 16:53:35 +0100

    bottoni

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const imagesArray = ["./img/01.jpg","./img/02.jpg","./img/03.jpg","./img/04.jpg","./img/05.jpg",];
 
-const itemsContainer = document.querySelector("row-items")
+const itemsContainer = document.querySelector(".row-items")
 
 for (let i = 0; i < imagesArray.length; i++) {
     const currentImage = imagesArray [i];
@@ -23,4 +23,53 @@ console.log(itemsArray);
 let activeItemIndex = 0;
 itemsArray[activeItemIndex].classList.add("active");
 
-//prendiamo da html row-items
+//gestisco il click sul bottone next
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+
+//devo gestire click quindi
+nextBtn.addEventListener("click", function() {
+
+    prevBtn.classList.remove("hidden")
+
+    if (activeItemIndex < (itemsArray.length - 1)) {
+    //devo mettere - 1 altrimenti mi farebbe fare un click in più
+
+        // rimuovi active dallo slide corrente
+        itemsArray[activeItemIndex].classList.remove("active");
+        
+        // incremento activeIndex
+        activeItemIndex++; //diventa 1. Tutto si concentra sull'incremento quindi su questa azione
+
+        //aggiungo active a quello nuovo quindi a 1 e poi il ciclo ricomincia per l'img successiva
+        itemsArray[activeItemIndex].classList.add("active");
+
+    //se siamo arrivati all'ultima slide
+    // nascondo il bottone infatti in css ho creato una classe chiamata hidden
+    if (activeItemIndex === itemsArray.length - 1) {
+        nextBtn.classList.add("hidden");
+    }
+}
+});
+
+//nascondiamo bottone di default
+prevBtn.classList.add("hidden");
+
+//da qui gestisco il click
+prevBtn.addEventListener("click", function () {
+
+    nextBtn.classList.remove("hidden");
+
+    //rimuovo active dalla slide prec
+    itemsArray[activeItemIndex].classList.remove("active");
+
+    //secrementare activeIndex
+    activeItemIndex--;
+
+    //aggiungere active a quello nuovo
+    itemsArray[activeItemIndex].classList.add("active");
+    if (activeItemIndex === 0) {
+        prevBtn.classList.add ("hidden")
+    }
+})
